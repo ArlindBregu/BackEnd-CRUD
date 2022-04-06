c6e074f59b9842ac27b9aef68deeec893209c954
lai2-01 <lai2-01@itismeucci.com>
2022-04-06 13:46:33 +0200

    secondo commit

--- index.php
+++ index.php
@@ -3,16 +3,52 @@
     <head></head>
     <body>
         <?php
-            // var_dump($_SERVER['REQUEST_METHOD']);
+            require('pages/db.php');
             $_metodoClient = $_SERVER['REQUEST_METHOD'];
-            if($_metodoClient == "GET"){
-                echo "è arrivato un GET";
-            }else if($_metodoClient == "POST"){
-                echo "è arrivato un POST";
-            }else if($_metodoClient == "PUT"){
-                echo "è arrivato un PUT";
-            }else if($_metodoClient == "DELETE"){
-                echo "è arrivato un DELETE";
+            $array = array(
+                "_embedded" => "employees"
+            );
+
+            switch($_metodoClient){
+                case 'GET':
+                    $query = "SELECT * FROM employees ORDER BY id LIMIT 0,20";
+                    $result = $mysqli->query($query);
+                    while ($row= $result-> fetch_assoc()) {
+                        $array = ["_embedded"]["employees"] = [
+                                [
+                                    'id' => $row["id"],
+                                    'birthDate' => $row["birth_date"],
+                                    'firstName' => $row["first_name"],
+                                    'lastName' => $row["last_name"],
+                                    'gender' => $row["gender"],
+                                    'hireDate' => $row["hire_date"]
+                                ]
+                        ];
+                    }
+                    /*$array[
+                        "_links"
+                        => "first",
+                        => "self",
+                        => "next",
+                        => "last",
+                        => "profile"
+                    ];
+                    $array[
+                        "page"
+                        => "size",
+                        => "totalElements",
+                        => "totalPages",
+                        => "number"
+                    ];*/
+
+                    echo json_encode($array);
+                    break;
+                case 'POST':
+                    echo "è arrivato un POST";
+                case 'PUt':
+                    echo "è arrivato un PUT";
+                case 'DELETE':
+                    echo "è arrivato un DELETE";
             }
         ?>
     </body>
